b12e406f971c2a6de38181169e11027aa454af52
amankeldi.kurban <dimitesting31@gmail.com>
2024-11-18 17:59:25 +0200

    Setting up the boiler plate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CustomCache
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Setting up the template");
+        }
+    }
+}
